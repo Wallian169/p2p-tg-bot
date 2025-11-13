4e08de59f5201e5e9f2acf97b90fd45b3f634d66
Vadym Vyshnevskyi <wallian169@gmail.com>
2025-11-13 22:03:59 +0200

    fix(models): fix and extend Order model

--- app/models.py
+++ app/models.py
@@ -2,17 +2,29 @@ from datetime import datetime
 from decimal import Decimal
 from enum import Enum as PyEnum
 
-from sqlalchemy import String, ForeignKey, Enum, Numeric, DateTime, func
+from sqlalchemy import String, ForeignKey, Enum, Numeric, DateTime, func, Column, Table
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
 
 
 class Base(DeclarativeBase):
     pass
 
-class Picture(Base):
-    __tablename__ = 'pictures'
+class PaymentMethod(Base):
+    __tablename__ = "payment_methods"
     id: Mapped[int] = mapped_column(primary_key=True)
-    path: Mapped[str] = mapped_column(String(255), nullable=False)
+    name: Mapped[str] = mapped_column(String(30), nullable=False, unique=True)
+
+    orders: Mapped[list["Order"]] = relationship(
+        secondary="order_payment_methods",
+        back_populates="payment_methods",
+    )
+
+order_payment_methods = Table(
+    "order_payment_methods",
+    Base.metadata,
+    Column("order_id", ForeignKey("orders.id"), primary_key=True),
+    Column("payment_method_id", ForeignKey("payment_methods.id"), primary_key=True)
+)
 
 class Currency(Base):
     __tablename__ = "currencies"
@@ -20,7 +32,7 @@ class Currency(Base):
     id: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[str] = mapped_column(String(100), unique=True, nullable=False)
     symbol: Mapped[str] = mapped_column(String(10), nullable=False)
-    icon: Mapped[str | None] = mapped_column(String, nullable=True)  # path to picture
+    icon: Mapped[str | None] = mapped_column(String, nullable=True)
 
 class User(Base):
     __tablename__ = "users"
@@ -43,6 +55,11 @@ class Order(Base):
     action: Mapped[OrderAction] = mapped_column(Enum(OrderAction, name="orderAction"), nullable=False)
     currency_id: Mapped[int] = mapped_column(ForeignKey("currencies.id"))
     amount: Mapped[Decimal] = mapped_column(Numeric(10, 2), nullable=False)
+    description: Mapped[str | None] = mapped_column(String(200), nullable=True)
+    payment_methods: Mapped[list[PaymentMethod]] = relationship(
+        secondary=order_payment_methods,
+        back_populates="orders",
+    )
     created_at: Mapped[datetime] = mapped_column(
         DateTime(timezone=True),
         server_default=func.now(),
@@ -55,5 +72,5 @@ class Order(Base):
         nullable=False,
     )
 
-    owner = relationship("User", lazy="joined")
-    currency_obj = relationship("Currency", lazy="joined")
+    owner = relationship("User")
+    currency_obj = relationship("Currency")
