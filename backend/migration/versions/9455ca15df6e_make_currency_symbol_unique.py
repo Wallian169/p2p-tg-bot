d9a512d64e313f771e34328e006dd4a1eab79a70
Vadym Vyshnevskyi <wallian169@gmail.com>
2026-02-08 18:20:37 +0200

    feat: add new migrations

--- backend/migration/versions/9455ca15df6e_make_currency_symbol_unique.py
+++ backend/migration/versions/9455ca15df6e_make_currency_symbol_unique.py
@@ -5,17 +5,15 @@ Revises: aeae2f66196f
 Create Date: 2026-02-03 09:32:53.247378
 
 """
-from typing import Sequence, Union
+from collections.abc import Sequence
 
 from alembic import op
-import sqlalchemy as sa
-
 
 # revision identifiers, used by Alembic.
 revision: str = '9455ca15df6e'
-down_revision: Union[str, Sequence[str], None] = 'aeae2f66196f'
-branch_labels: Union[str, Sequence[str], None] = None
-depends_on: Union[str, Sequence[str], None] = None
+down_revision: str | Sequence[str] | None = 'aeae2f66196f'
+branch_labels: str | Sequence[str] | None = None
+depends_on: str | Sequence[str] | None = None
 
 
 def upgrade() -> None:
--- backend/migration/versions/aeae2f66196f_add_deal_model.py
+++ backend/migration/versions/aeae2f66196f_add_deal_model.py
@@ -5,17 +5,16 @@ Revises: c4c7ce5127d0
 Create Date: 2026-02-03 08:43:25.347267
 
 """
-from typing import Sequence, Union
+from collections.abc import Sequence
 
-from alembic import op
 import sqlalchemy as sa
-
+from alembic import op
 
 # revision identifiers, used by Alembic.
 revision: str = 'aeae2f66196f'
-down_revision: Union[str, Sequence[str], None] = 'c4c7ce5127d0'
-branch_labels: Union[str, Sequence[str], None] = None
-depends_on: Union[str, Sequence[str], None] = None
+down_revision: str | Sequence[str] | None = 'c4c7ce5127d0'
+branch_labels: str | Sequence[str] | None = None
+depends_on: str | Sequence[str] | None = None
 
 
 def upgrade() -> None:
